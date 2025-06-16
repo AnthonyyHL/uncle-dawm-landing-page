b7a60e3bf8f62e215fc4db91f54d5f54496028e2
AnthonyyHL <anthleon@espol.edu.ec>
2025-06-16 10:41:44 -0500

    feat: Implement API for landing page images

---
+++ src/js/api-fetch.js
@@ -0,0 +1,47 @@
+const Images = {
+    home_img: "right-image",
+    bathroom_img: "bathroom-ceramics",
+    kitchen_img: "kitchen-ceramics",
+    tiles_img: "tiles",
+    stones_img: "natural-stones"
+}
+
+async function getApiKey() {
+    return import.meta.env.VITE_PEXELS_API_KEY;
+}
+
+async function getImage(topic, index = 0) {
+    const apiKey = await getApiKey();
+
+    try {
+        const request = await fetch(`https://api.pexels.com/v1/search?query=${topic}`, {
+            headers: {
+                Authorization: apiKey
+            }
+        });
+
+        const response = await request.json();
+        console.log(topic, ": ", response)
+        const imageURL = response["photos"][index]["src"]["large"];
+        return imageURL;
+    } catch (error) {
+        console.error('Error: ', error);
+    }
+}
+
+(async () => {
+    const home_img = document.querySelector(`#${Images.home_img} img`);
+    home_img.src = await getImage("home interior")
+
+    const bathroom_img = document.querySelector(`#${Images.bathroom_img} img`);
+    bathroom_img.src = await getImage("bathroom ceramics")
+    
+    const kitchen_img = document.querySelector(`#${Images.kitchen_img} img`);
+    kitchen_img.src = await getImage("kitchen")
+
+    const tiles_img = document.querySelector(`#${Images.tiles_img} img`);
+    tiles_img.src = await getImage("tiles")
+    
+    const stones_img = document.querySelector(`#${Images.stones_img} img`);
+    stones_img.src = await getImage("marmol ceramic floor", 3)
+})();
